0087a70d686c714658e9e4b72586050f828c9770
Butkhoy <144401416+Butkhoy@users.noreply.github.com>
2025-02-05 12:07:08 -0700

    UPDATED code

--- database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
+++ database/migrations/2014_10_12_100000_create_password_reset_tokens_table.php
@@ -11,9 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        if (!Schema::hasTable('password_reset_tokets')) {
+        if (!Schema::hasTable('password_reset_tokens')) { // Fixed typo
             Schema::create('password_reset_tokens', function (Blueprint $table) {
-                $table->string('email');->primary(); 
+                $table->string('email')->primary(); // Removed incorrect semicolon
                 $table->string('token');
                 $table->timestamp('created_at')->nullable();
             });
